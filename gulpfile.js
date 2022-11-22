1c7e342931510d82194bb8cffeefb7223fa4013a
IrynaPoritska <115899969+IrynaPoritska@users.noreply.github.com>
2022-11-22 17:59:12 +0200

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,290 @@
-const { src, dest, watch, parallel } = require('gulp');
+import gulp from "gulp";
+import * as nodePath from 'path';
+import replace from "gulp-replace";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
+import browserSync from "browser-sync";
+import newer from "gulp-newer";
+import ifPlugin from "gulp-if";
+import { deleteAsync } from "del";
+import fileInclude from "gulp-file-include";
+import webpHtmlNosvg from "gulp-webp-html-nosvg";
+import versionNumber from "gulp-version-number";
+import webpack from "webpack-stream";
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import rename from 'gulp-rename';
+import cleanCss from 'gulp-clean-css';
+import webpcss from 'gulp-webpcss';
+import autoprefixer from 'gulp-autoprefixer';
+import groupCssMediaQueries from 'gulp-group-css-media-queries';
+import fs from "fs";
+import fonter from "gulp-fonter";
+import ttf2woff2 from "gulp-ttf2woff2";
 
-const scss         = require('gulp-sass')(require('sass'));
-const concat       = require('gulp-concat');
-const browserSync  = require('browser-sync').create();
-const uglify       = require('gulp-uglify-es').default;
-const autoprefixer = require('gulp-autoprefixer');
-const imagemin     = require('gulp-imagemin');
+const rootFolder = nodePath.basename(nodePath.resolve());
+const buildFolder = './dist';
+const srcFolder = './src';
 
-function browsersync() {
-    browserSync.init({
-        server : {
-            baseDir: 'app/'
-        }
+const path = {
+    build: {
+        js: `${buildFolder}/js/`,
+        css: `${buildFolder}/css/`,
+        html: `${buildFolder}/`,
+        images: `${buildFolder}/img/`,
+        fonts: `${buildFolder}/fonts/`,
+        files: `${buildFolder}/files/`
+    },
+    src: {
+        js: `${srcFolder}/js/app.js`,
+        images: `${srcFolder}/img/*.*`,
+        scss: `${srcFolder}/scss/style.scss`,
+        html: `${srcFolder}/*.html`,
+        files: `${srcFolder}/files/**/*.*`,
+    },
+    watch: {
+        js: `${srcFolder}/js/**/*.js`,
+        scss: `${srcFolder}/scss/**/*.scss`,
+        html: `${srcFolder}/**/*.html`,
+        images: `${srcFolder}/img/**/*.{jpg, jpeg, png, svg, gif, ico, webp}`,
+        files: `${srcFolder}/files/**/*.*`
+    },
+    clean: buildFolder,
+    buildFolder: buildFolder,
+    srcFolder: srcFolder,
+    rootFolder: rootFolder,
+    ftp: `test`
+}
+
+
+const plugins = {
+    replace: replace,
+    plumber: plumber,
+    notify: notify,
+    browserSync: browserSync,
+    newer: newer,
+    if: ifPlugin
+}
+global.app = {
+    isBuild: process.argv.includes('--build'),
+    isDev: !process.argv.includes('--build'),
+    path: path,
+    gulp: gulp,
+    plugins: plugins
+}
+
+const copy = () => {
+    return app.gulp.src(app.path.src.files)
+        .pipe(app.gulp.dest(app.path.build.files));
+}
+const reset = () => {
+    return deleteAsync(app.path.clean);
+}
+
+const html = () => {
+    return app.gulp.src(app.path.src.html)
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "HTML",
+                massage: "Error: <%= error.massage %>"
+            })
+        ))
+        .pipe(fileInclude())
+        .pipe(app.plugins.replace(/@img\//g, 'img/'))
+        .pipe(app.plugins.if(
+                app.isBuild,
+                webpHtmlNosvg()
+            )
+        )
+        .pipe(app.plugins.if(
+            app.isBuild,
+            versionNumber({
+                'value': '%DT%',
+                'append': {
+                    'key': '_v',
+                    'cover': 0,
+                    'to': [
+                        'css',
+                        'js'
+                    ]
+                },
+                'output': {
+                    'file': 'gulp/version.json'
+                }
+            })
+        ))
+        .pipe(app.gulp.dest(app.path.build.html))
+        .pipe(app.plugins.browserSync.stream());
+}
+const server = (done) => {
+    app.plugins.browserSync.init({
+        server: {
+            baseDir: `${app.path.build.html}`
+        },
+        notify: true,
+        port: 3000
     });
 }
 
-function images(){
-    return src('app/images/**/*')
-    .pipe(imagemin([
-        imagemin.gifsicle({interlaced: true}),
-        imagemin.mozjpeg({quality: 75, progressive: true}),
-        imagemin.optipng({optimizationLevel: 5}),
-        imagemin.svgo({
-            plugins: [
-                {removeViewBox: true},
-                {cleanupIDs: false}
-            ]
-        })
-    ]))
-    .pipe(dest('dist/images'))
+const sass = gulpSass(dartSass);
+const scss = () => {
+    return app.gulp.src(app.path.src.scss, { sourcemaps: app.isDev })
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "SCSS",
+                massage: "Error: <%= error.massage %>"
+            })))
+        .pipe(app.plugins.replace(/@img\//g, '../img/'))
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(app.plugins.if(
+                app.isBuild,
+                groupCssMediaQueries()
+            )
+        )
+        .pipe(app.plugins.if(
+                app.isBuild,
+                webpcss({
+                    webpClass: ".webp",
+                    noWebpClass: ".no-webp"
+                })
+            )
+        )
+        .pipe(app.plugins.if(
+                app.isBuild,
+                autoprefixer({
+                    grid: true,
+                    overrideBrowserslist: ["last 3 versions"],
+                    cascade: true
+                })
+            )
+        )
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(app.plugins.if(
+                app.isBuild,
+                cleanCss()
+            )
+        )
+        .pipe(rename({
+            extname: ".min.css"
+        }))
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(app.plugins.browserSync.stream());
+}
+const js = () => {
+    return app.gulp.src(app.path.src.js, {sourcemaps: true})
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "JS",
+                massage: "Error: <%= error.massage %>"
+            })
+        ))
+        .pipe(webpack({
+            mode: app.isBuild ? 'production' : 'development',
+            output: {
+                filename: 'app.min.js'
+            }
+        }))
+        .pipe(app.gulp.dest(app.path.build.js))
+        .pipe(app.plugins.browserSync.stream());
+}
+const images = () => {
+    return app.gulp.src(app.path.src.images)
+        .pipe(app.gulp.dest(app.path.build.images))
+        .pipe(app.plugins.browserSync.stream());
 }
 
-function scripts (){
-    return src([
-        'node_modules/jquery/dist/jquery.js',
-        'app/js/main.js'
-    ])
-    .pipe(concat('main.min.js'))
-    .pipe(uglify())
-    .pipe(dest('app/js'))
-    .pipe(browserSync.stream())
-}
-function styles() {
-    return src('app/scss/style.scss')
-    .pipe(scss({outputStyle: 'compressed'}))
-    .pipe(concat('style.min.css'))
-    .pipe(autoprefixer({
-        overrideBrowserslist: ['last 10 version'],
-        grid: true
-    }))
-    .pipe(dest('app/css'))
-    .pipe(browserSync.stream())
+const otfToTtf = () => {
+    return app.gulp.src(`${app.path.srcFolder}/fonts/*.otf`, {})
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "FONTS",
+                massage: "Error: <%= error.massage %>"
+            })
+        ))
+        .pipe(fonter({
+            formats: ['ttf']
+        }))
+        .pipe(app.gulp.dest(`${app.path.srcFolder}/fonts/`));
 }
 
-function build(){
-    return src([
-        'app/css/style.min.css',
-        'app/fonts/**/*',
-        'app/js/main.min.js',
-        'app/*.html'
-    ], {base: 'app'})
-    .pipe(dest('dist'))
+const ttfToWoff = () => {
+    return app.gulp.src(`${app.path.srcFolder}/fonts/*.ttf`, {})
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "FONTS",
+                massage: "Error: <%= error.massage %>"
+            })
+        ))
+        .pipe(fonter({
+            formats: ['woff']
+        }))
+        .pipe(app.gulp.dest(`${app.path.build.fonts}`))
+        .pipe(app.gulp.src(`${app.path.srcFolder}/fonts/*.ttf`))
+        .pipe(ttf2woff2())
+        .pipe(app.gulp.dest(`${app.path.build.fonts}`));
 }
 
-function watching(){
-    watch(['app/scss/**/*.scss'], styles);
-    watch(['app/js/**/*.js','!app/js/main.min.js'], scripts);
-    watch(['app/*.html']).on('change', browserSync.reload);
+const fontsStyle = () => {
+    let fontsFile = `${app.path.srcFolder}/scss/fonts.scss`;
+    fs.readdir(app.path.build.fonts, function(err, fontsFiles){
+        if(fontsFiles) {
+            if(!fs.existsSync(fontsFile)) {
+                fs.writeFile(fontsFile, '', cb);
+                let newFileOnly;
+                for (let i = 0; i < fontsFiles.length; i++) {
+                    let fontFileName = fontsFiles[i].split('.')[0];
+                    if (newFileOnly !== fontFileName) {
+                        let fontName = fontFileName.split('-')[0] ? fontFileName.split('-')[0] : fontFileName;
+                        let fontWeight = fontFileName.split('-')[1] ? fontFileName.split('-')[1] : fontFileName;
+                        if (fontWeight.toLowerCase() === 'thin') {
+                            fontWeight = 100;
+                        } else if (fontWeight.toLowerCase() === 'extralight') {
+                            fontWeight = 200;
+                        } else if (fontWeight.toLowerCase() === 'light') {
+                            fontWeight = 300;
+                        } else if (fontWeight.toLowerCase() === 'medium') {
+                            fontWeight = 500;
+                        } else if (fontWeight.toLowerCase() === 'semibold') {
+                            fontWeight = 600;
+                        } else if (fontWeight.toLowerCase() === 'bold') {
+                            fontWeight = 700;
+                        } else if (fontWeight.toLowerCase() === 'extrabold' || fontWeight.toLowerCase() === 'heavy') {
+                            fontWeight = 800;
+                        } else if (fontWeight.toLowerCase() === 'black') {
+                            fontWeight = 900;
+                        } else {
+                            fontWeight = 400;
+                        }
+                        fs.appendFile(fontsFile, `@font-face{\n\tfont-family: ${fontName};\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`, cb);
+                        newFileOnly = fontFileName;
+                    }
+                }
+
+            }
+        }
+    });
+    return app.gulp.src(`${app.path.srcFolder}`);
+    function cb() { }
+}
+function watcher() {
+    gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.scss, scss);
+    gulp.watch(path.watch.js, js);
+    gulp.watch(path.watch.images, images);
 }
 
-exports.styles = styles;
-exports.watching = watching;
-exports.browsersync = browsersync;
-exports.scripts = scripts;
-exports.build = build;
-exports.images = images;
 
-exports.default = parallel(scripts ,browsersync , watching);
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
+
+const mainTasks = gulp.parallel(fonts, gulp.parallel(copy, html, scss, js, images));
+
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+const build = gulp.series(reset, mainTasks);
+
+export { dev }
+export { build }
+
+gulp.task('default', dev);
